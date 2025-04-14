e087ab4d7eeacd2bd79ff320ec8963faec120441
Ran-j <ran-junior@hotmail.com>
2025-04-14 01:43:20 -0300

    feat: added missing header in stub function

--- ps2xRecomp/src/ps2_recompiler.cpp
+++ ps2xRecomp/src/ps2_recompiler.cpp
@@ -289,7 +289,8 @@ namespace ps2recomp
         std::stringstream ss;
 
         ss << "#include \"ps2_runtime_macros.h\"\n";
-        ss << "#include \"ps2_runtime.h\"\n\n";
+        ss << "#include \"ps2_runtime.h\"\n";
+        ss << "#include \"ps2_recompiled_functions.h\"\n\n"; 
 
         ss << "// STUB FUNCTION: " << function.name << "\n";
         ss << "// Address: 0x" << std::hex << function.start << " - 0x" << function.end << std::dec << "\n";
