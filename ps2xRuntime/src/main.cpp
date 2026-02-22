dfe1adec6b8e97dade50aba97514d7886e3128fb
DanielSvoboda <28203966+DanielSvoboda@users.noreply.github.com>
2026-02-22 15:07:03 -0300

    Add game title resolution from internal database

--- ps2xRuntime/src/main.cpp
+++ ps2xRuntime/src/main.cpp
@@ -1,9 +1,54 @@
 #include "ps2_runtime.h"
 #include "register_functions.h"
+#include "games_database.h"
+
 #include <iostream>
 #include <string>
+#include <filesystem>
+#include <fstream>
+#include <unordered_map>
 
-int main(int argc, char *argv[])
+std::unordered_map<std::string, std::string> loadGameDatabase(const std::string& path)
+{
+    std::unordered_map<std::string, std::string> database;
+    std::ifstream file(path);
+
+    if (!file.is_open())
+    {
+        return database;
+    }
+
+    std::string line;
+    while (std::getline(file, line))
+    {
+        size_t sep = line.find('|');
+        if (sep != std::string::npos)
+        {
+            std::string id = line.substr(0, sep);
+            std::string name = line.substr(sep + 1);
+            database[id] = name;
+        }
+    }
+
+    return database;
+}
+
+std::string normalizeGameId(const std::string& folderName)
+{
+    std::string result = folderName;
+
+    size_t underscore = result.find('_');
+    if (underscore != std::string::npos)
+        result[underscore] = '-';
+
+    size_t dot = result.find('.');
+    if (dot != std::string::npos)
+        result.erase(dot, 1);
+
+    return result;
+}
+
+int main(int argc, char* argv[])
 {
     if (argc < 2)
     {
@@ -12,9 +57,25 @@ int main(int argc, char *argv[])
     }
 
     std::string elfPath = argv[1];
+    std::filesystem::path pathObj(elfPath);
+    std::string folderName = pathObj.filename().string();
+    std::string normalizedId = normalizeGameId(folderName);
+    auto gameDatabase = loadGameDatabase("games.txt");
+    std::string windowTitle = "PS2-Recomp | ";
+
+    const char* gameName = getGameName(normalizedId);
+
+    if (gameName)
+    {
+        windowTitle += gameName;
+    }
+    else
+    {
+        windowTitle += folderName;
+    }
 
     PS2Runtime runtime;
-    if (!runtime.initialize("ps2xRuntime (Raylib host)"))
+    if (!runtime.initialize(windowTitle.c_str()))
     {
         std::cerr << "Failed to initialize PS2 runtime" << std::endl;
         return 1;
